0a8d65a1c474ad81f4420beb9cbdeb02a1edf863
Isa <costabileisa@gmail.com>
2023-05-25 14:04:50 -0400

    render debug

--- Rakefile
+++ Rakefile
@@ -1,3 +1,13 @@
+# Rakefile
+
+task "assets:precompile" do
+  # Task implementation goes here
+end
+
+task "assets:clean" do
+  # Task implementation goes here
+end
+
 require_relative "config/application"
 
 Rails.application.load_tasks
--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,5 +1,5 @@
 class FallbackController < ActionController::Base
   def index
-    render file: 'public/index.html'
+    render file: "/Users/isabellacostabile/Development/Flatiron-School/phase-5/phase-5-project/client/public/index.html"
   end
 end
