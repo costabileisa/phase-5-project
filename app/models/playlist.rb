5fce4662a1b39727e5b3080dd051b7a872af81b6
Isa <costabileisa@gmail.com>
2023-05-25 16:11:04 -0400

    update models

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,2 +1,8 @@
 class Playlist < ApplicationRecord
+  belongs_to :user
+  has_many :songs, dependent: :destroy
+  has_many :artists, through: :songs
+  has_many :albums, through: :songs
+
+  has_one_attached :cover_blob
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  
+  has_many :playlists
+
+  validates :username, uniqueness: true
+  validates :username, length: { minimum: 3 }
+  validates :password, length: { in: 6..20 }
+  validates :username, :email, :birthdate, :region, :avatar_url, presence: true
 end
