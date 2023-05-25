1c0b0955028f9047d0c82cbabf9eb9368c6f2923
Isa <costabileisa@gmail.com>
2023-05-25 14:10:12 -0400

    change path for render

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,5 +1,5 @@
 class FallbackController < ActionController::Base
   def index
-    render file: "/Users/isabellacostabile/Development/Flatiron-School/phase-5/phase-5-project/client/public/index.html"
+    render file: "client/public/index.html"
   end
 end
